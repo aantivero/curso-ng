882b23a6eb388f2568449fde0cf621ece70395b6
alejandroantivero <alejandro.antivero@gmail.com>
2017-08-17 17:38:06 -0300

    agregamos styles bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>Data Binding</h1>
-  <app-lifecycle *ngIf="!delete" [vinculado]="vinculante">
-    <p #contenido>{{test}}</p>
-  </app-lifecycle>
-  <button (click) = "delete = true">Ocultar</button>
-  <button (click) = "delete = false">Ver</button>
-  <button (click) = "vinculante = vinculante + 10">Incrementar</button>
-  `,
-  styles: [
-    `h1 {
-      color: black
-    }`
-  ]
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   title = 'app';
